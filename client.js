02f4bf7ccee0820376a3803f57e0aa17c7eb5421
ElisaBerisha <elisaberisha888@gmail.com>
2024-05-18 18:56:59 -0700

    Update client.js

--- client.js
+++ client.js
@@ -16,3 +16,18 @@ client.connect(1337, '127.0.0.1', function() {
         let signed_msg = sign(msg, `${__dirname}/certificates/client/client-private.key`)
 
         let encrypted_msg = encrypt(msg, __dirname + '/certificates/client/server-publickey.cer')
+        
+        client.write(encrypted_msg.toString('base64') + '.' + signed_msg.toString('base64'));
+        readline.close();
+    });
+});
+
+client.on('data', function(msg) {
+    let decrypted_msg = decrypt(msg, __dirname + '/certificates/client/client-private.key', '1234')
+    console.log("Decrypted: " +  decrypted_msg)
+    client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+    console.log('Connection closed');
+});
