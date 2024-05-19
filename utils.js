0519d7294ce1bb0af2cef94d4c4703abd1318d6d
ElisaGjurkaj <elisa.gjrk@gmail.com>
2024-05-19 22:36:46 +0200

    Update

--- utils.js
+++ utils.js
@@ -25,3 +25,25 @@ const decrypt = function(toDecrypt, relativeOrAbsolutePathtoPrivateKey, _passphr
   )
   return decrypted.toString('utf8')
 }
+
+const sign = function(toSign, relativeOrAbsolutePathtoPrivateKey){
+
+  const absolutePath = path.resolve(relativeOrAbsolutePathtoPrivateKey)
+  const publicKey = fs.readFileSync(absolutePath, 'utf8')
+
+  const buffer = Buffer.from(toSign)
+
+  return crypto.sign('sha512', buffer , publicKey);
+
+}
+
+const verify = function(toVerify, relativeOrAbsolutePathToPublicKey, signature){
+
+  const absolutePath = path.resolve(relativeOrAbsolutePathToPublicKey)
+  const privateKey = fs.readFileSync(absolutePath, 'utf8')
+  const buffer = Buffer.from(toVerify)
+
+  return crypto.verify('sha512', buffer, privateKey, signature);
+}
+
+ module.exports = { encrypt, decrypt, sign, verify }
